b23b8c298c10e65eac6f552bca54111318d942c9
Herobroto <herobrotelpro@gmail.com>
2026-07-28 14:39:58 -0600

    deleted onLivingDrops event

--- src/main/java/com/herobrot/scalingdifficulty/events/EntityEvents.java
+++ src/main/java/com/herobrot/scalingdifficulty/events/EntityEvents.java
@@ -2,7 +2,6 @@ package com.herobrot.scalingdifficulty.events;
 
 import com.herobrot.scalingdifficulty.ScalingDifficulty;
 import com.herobrot.scalingdifficulty.api.DifficultyCalculator;
-import com.herobrot.scalingdifficulty.compat.LevelplateCompat;
 import com.herobrot.scalingdifficulty.data.ModAttachments;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.DamageTypeTags;
@@ -10,14 +9,11 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Guardian;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
-import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.neoforge.event.entity.living.LivingExperienceDropEvent;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
@@ -28,12 +24,11 @@ public class EntityEvents {
     public static void onEntityJoinLevel(EntityJoinLevelEvent event) {
         if (event.getLevel().isClientSide()) return;
 
-        if (event.getEntity() instanceof Mob mob) {
+        if (event.getEntity() instanceof Mob mob && event.getLevel() instanceof ServerLevel serverLevel) {
             boolean isProcessed = mob.getData(ModAttachments.PROCESSED);
             if (isProcessed) return;
-
             mob.setData(ModAttachments.PROCESSED, true);
-            DifficultyCalculator.applyScaling(mob, (ServerLevel) event.getLevel());
+            DifficultyCalculator.applyScaling(mob, serverLevel);
         }
     }
 
@@ -45,42 +40,6 @@ public class EntityEvents {
             if (multiplier > 1.0f) {
                 float cappedMultiplier = Math.min(multiplier, ScalingDifficulty.CONFIG.maxXPFactor);
                 event.setDroppedExperience((int) (event.getDroppedExperience() * cappedMultiplier));
-            }
-        }
-    }
-
-    @SubscribeEvent
-    @SuppressWarnings("resource")
-    public static void onLivingDrops(LivingDropsEvent event) {
-        if (!ScalingDifficulty.CONFIG.dropMoreLoot) return;
-        if (!(event.getEntity() instanceof Mob mob)) return;
-
-        float dropChance;
-
-        if (ScalingDifficulty.isLevelplateLoaded) {
-            int mobLevel = LevelplateCompat.getMobLevel(mob);
-            if (mobLevel <= 1) return;
-            dropChance = mobLevel * ScalingDifficulty.CONFIG.moreLootChance;
-        } else {
-            float multiplier = mob.getData(ModAttachments.DIFFICULTY_MULTIPLIER);
-            if (multiplier <= 1.0f) return;
-            int simulatedLevel = (int) (10 * multiplier - 10);
-            if (simulatedLevel < 1) simulatedLevel = 1;
-            dropChance = simulatedLevel * ScalingDifficulty.CONFIG.moreLootChance;
-        }
-
-        dropChance = Math.min(dropChance, ScalingDifficulty.CONFIG.maxLootChance);
-
-        if (mob.level().random.nextFloat() <= dropChance) {
-            for (ItemEntity drop : event.getDrops()) {
-                if (mob.level().random.nextFloat() >= ScalingDifficulty.CONFIG.chanceForEachItem) {
-                    ItemStack stack = drop.getItem();
-                    int bonus = (int) (stack.getCount() * dropChance);
-                    if (bonus > 0) {
-                        stack.grow(bonus);
-                        drop.setItem(stack);
-                    }
-                }
             }
         }
     }
@@ -104,13 +63,11 @@ public class EntityEvents {
             if (isIndirectOrSpecial) {
                 float damageFactor = mob.getData(ModAttachments.DIFFICULTY_MULTIPLIER);
 
-                if (mob instanceof Creeper) {
+                if (mob instanceof Creeper)
                     damageFactor *= (float) ScalingDifficulty.CONFIG.creeperExplosionFactor;
-                }
 
-                if (damageFactor > 1.0f) {
+                if (damageFactor > 1.0f)
                     event.setAmount(event.getAmount() * damageFactor);
-                }
             }
         }
     }
